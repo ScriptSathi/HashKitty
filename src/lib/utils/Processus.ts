e9457ee1a4981a61b3225b6baa753bc501ce0d44
Tristan D'audi <tristan.daudibert@gmail.com>
2022-12-04 19:07:58 +0100

    refacto Processus

--- src/lib/utils/Processus.ts
+++ src/lib/utils/Processus.ts
@@ -1,65 +1,70 @@
-import { spawn } from 'child_process';
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
 import { parentPort } from 'node:worker_threads';
 
 import { HashcatError } from '../hashcat/HashcatError';
 import { logger } from './Logger';
-import { THashcatStatus } from '../types/THashcat';
+import { THashcatPartialStatus } from '../types/THashcat';
 
 export type TProcessStdout = {
-    status: THashcatStatus | undefined;
+    status: THashcatPartialStatus | undefined;
     any: string | undefined;
 };
 
 export class Processus {
-    public static spawn(stringCommand: string): void {
-        const cmd: string[] = stringCommand.split(' ').filter(n => n);
+    private cmd!: string[];
+    private proc!: ChildProcessWithoutNullStreams;
 
-        const args: string[] = cmd.splice(1);
-        const proc = spawn(cmd[0], args);
+    public spawn(stringCommand: string): void {
+        this.cmd = stringCommand.split(' ').filter(n => n);
+        const args: string[] = this.cmd.splice(1);
+        this.proc = spawn(this.cmd[0], args);
         logger.debug(`Executed command: ${stringCommand}`);
-        parentPort &&
-            parentPort.once('message', message => {
-                if (message === 'exit') {
-                    proc.kill();
-                }
-            });
 
-        proc.stderr &&
-            proc.stderr.on('data', (data: Buffer) => {
-                logger.error(Processus.checkStderr(data.toString().trim()));
-            });
+        parentPort && parentPort.once('message', this.onParentProcessMessage);
 
-        proc.stdout &&
-            proc.stdout.on('data', (data: Buffer) => {
-                try {
-                    const sessionStatus: THashcatStatus = JSON.parse(
-                        data.toString().trim()
-                    );
-                    parentPort?.postMessage({ status: sessionStatus });
-                } catch (e) {
-                    parentPort?.postMessage({
-                        any: data.toString().trim(),
-                    });
-                }
-                setTimeout(() => {
-                    proc.stdin.write('\n');
-                }, 1000);
-            });
-
-        proc.on('close', (code: number) => {
-            if (code !== 0 && code !== null && code !== 1) {
-                logger.error(
-                    new Error(`Command '${cmd}' failed with code ${code}`)
-                );
-            } else if (code === null) {
-                logger.info(
-                    `A request has been sent to stop the process: ${cmd[0]}`
-                );
-            } else if (code === 1) {
-                logger.info(`Process: ${cmd[0]} ended correctly`);
-            }
-        });
+        this.proc.stderr && this.proc.stderr.on('data', this.onStderr);
+        this.proc.stdout && this.proc.stdout.on('data', this.onStdout);
+        this.proc.on('close', this.onClose);
     }
+
+    private onParentProcessMessage = (message: string): void => {
+        if (message === 'exit') {
+            this.proc.kill();
+        }
+    };
+    private onStderr = (data: Buffer): void => {
+        logger.error(Processus.checkStderr(data.toString().trim()));
+    };
+
+    private onStdout = (data: Buffer): void => {
+        try {
+            const sessionStatus: THashcatPartialStatus = JSON.parse(
+                data.toString().trim()
+            );
+            parentPort?.postMessage({ status: sessionStatus });
+        } catch (e) {
+            parentPort?.postMessage({
+                any: data.toString().trim(),
+            });
+        }
+        setTimeout(() => {
+            this.proc.stdin.write('\n');
+        }, 1000);
+    };
+
+    private onClose = (code: number): void => {
+        if (code !== 0 && code !== null && code !== 1) {
+            logger.error(
+                new Error(`Command '${this.cmd}' failed with code ${code}`)
+            );
+        } else if (code === null) {
+            logger.info(
+                `A request has been sent to stop the process: ${this.cmd[0]}`
+            );
+        } else if (code === 1) {
+            logger.info(`Process: ${this.cmd[0]} ended correctly`);
+        }
+    };
 
     private static checkStderr(stderr: string): HashcatError {
         if (stderr.match(/No hashes loaded/)) {
@@ -79,7 +84,7 @@ export class Processus {
 (() => {
     if (parentPort) {
         parentPort.once('message', cmd => {
-            Processus.spawn(cmd);
+            new Processus().spawn(cmd);
         });
     }
 })();
