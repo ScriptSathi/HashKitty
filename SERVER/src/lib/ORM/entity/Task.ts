8bd93a4a14f78bf93311d40bf7f265b5bcb6ea9c
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-09 15:38:40 +0100

    Make entites working fine

--- SERVER/src/lib/ORM/entity/Task.ts
+++ SERVER/src/lib/ORM/entity/Task.ts
@@ -2,6 +2,7 @@
 import { Options } from './Options';
 import { HashType } from './HashType';
 import { TemplateTask } from './TemplateTask';
+import { Hashlist } from './Hashlist';
 import {
     PrimaryGeneratedColumn,
     Entity,
@@ -20,7 +21,7 @@ export class Task {
     name!: string;
 
     @Column('varchar', { nullable: true })
-    description!: string;
+    description?: string;
 
     @OneToOne(() => Options, { nullable: false })
     @JoinColumn({ name: 'options_id', referencedColumnName: 'id' })
@@ -31,11 +32,15 @@ export class Task {
         (templateTask: TemplateTask) => templateTask.id
     )
     @JoinColumn({ name: 'template_task_id', referencedColumnName: 'id' })
-    templateTaskId!: TemplateTask | null;
+    templateTaskId?: number | null;
 
     @ManyToOne(() => HashType, (hashType: HashType) => hashType.id)
     @JoinColumn({ name: 'hashtype_id', referencedColumnName: 'id' })
     hashTypeId!: number;
+
+    @ManyToOne(() => Hashlist, (hashlist: Hashlist) => hashlist.id)
+    @JoinColumn({ name: 'hashlist_id', referencedColumnName: 'id' })
+    hashlistId!: number;
 
     @Column('date', { default: new Date(), name: 'created_at' })
     createdAt!: Date;
@@ -44,8 +49,8 @@ export class Task {
     lastestModification!: Date;
 
     @Column('date', { nullable: true, name: 'ended_at' })
-    endeddAt!: Date;
+    endeddAt?: Date;
 
     @Column('bool', { nullable: true, name: 'is_finished' })
-    isfinished!: Boolean;
+    isfinished?: Boolean;
 }
