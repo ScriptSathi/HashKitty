407f82a21a7e94724632fe4d2477fae9f10a1de4
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-09 15:39:09 +0100

    End of factory, it generate data needed

---
+++ SERVER/src/lib/ORM/data/workloadProfiles.ts
@@ -0,0 +1,22 @@
+export const workloadProfile = [
+    {
+        profileId: 1,
+        powerConsumation: 'Low',
+        desktopImpac: 'Minimal',
+    },
+    {
+        profileId: 2,
+        powerConsumation: 'Economic',
+        desktopImpac: 'Noticeable',
+    },
+    {
+        profileId: 3,
+        powerConsumation: 'High',
+        desktopImpac: 'Unresponsive',
+    },
+    {
+        profileId: 4,
+        powerConsumation: 'Insane',
+        desktopImpac: 'Headless',
+    },
+];
