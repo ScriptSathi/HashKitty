d4441bb78d80a6c827a09fbc3cebb966254d8cc8
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-09 15:39:09 +0100

    End of factory, it generate data needed

---
+++ SERVER/src/lib/ORM/migration/Migration.ts
@@ -0,0 +1,39 @@
+import { DataSource } from 'typeorm';
+import { attackModes } from '../data/attackModes';
+import { hashTypes } from '../data/hashtypes';
+import { AttackMode } from '../entity/AttackMode';
+import { HashType } from '../entity/HashType';
+
+export class Migration {
+    private appDataSource: DataSource;
+
+    constructor(appDataSource: DataSource) {
+        this.appDataSource = appDataSource;
+    }
+
+    public migrateAll(): void {
+        this.migrateAttackModes();
+        this.migrateHashTypes();
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public migrateHashTypes = async (_ = 0): Promise<void> => {
+        for (const hashType of hashTypes) {
+            const dbHashType = new HashType();
+            dbHashType.typeNumber = parseInt(hashType.id);
+            dbHashType.name = hashType.name;
+            dbHashType.description = hashType.description;
+            await this.appDataSource.manager.save(dbHashType);
+        }
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public migrateAttackModes = async (_ = 0): Promise<void> => {
+        for (const attackMode of attackModes) {
+            const dbAttackMode = new AttackMode();
+            dbAttackMode.type = parseInt(attackMode.type);
+            dbAttackMode.name = attackMode.name;
+            await this.appDataSource.manager.save(dbAttackMode);
+        }
+    };
+}
