9ed1f05857cb30bbb226da5c1ada4f4b5c176fc2
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-14 14:22:25 +0100

    Card Task is partially done, yet there is no interaction with backend

--- hashkitty-front/src/components/CardTask.tsx
+++ hashkitty-front/src/components/CardTask.tsx
@@ -1,36 +1,151 @@
-import React, { Component, CSSProperties } from 'react';
+import React, { Component } from 'react';
+
 import { TTask } from '../types/TypesORM';
+import {
+    bottomBox,
+    bottomBoxText,
+    cardBodyGeneric,
+    moreDetails,
+    runButton,
+    shadow,
+    taskName,
+    taskSoftInfos,
+    topLeftPart,
+    topPart,
+} from '../styles/CardTask';
+import '../assets/styles/main.scss';
+import stopTask from '../assets/images/stopTask.svg';
+import stopTaskHover from '../assets/images/stopTaskHover.svg';
+import playTaskHover from '../assets/images/playTaskHover.svg';
+import playTask from '../assets/images/playTask.svg';
+import CSSProperties from 'react';
 
-class CardTask extends Component<TTask, TTask> {
-    // display: 'grid',
-    // height: 309,
-    // width: 445,
-    // backgroundColor: 'yellow',
-    // border: '1px solid',
-
-    private main: CSSProperties = {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: 'grid',
-        height: 309,
-        width: 445,
-        backgroundColor: 'red',
-        border: '1px solid',
+export default class CardTask extends Component<
+    TTask,
+    {
+        mouseIsEnterTaskCard: boolean;
+        mouseIsEnterRunTask: boolean;
+        clickedRunTask: boolean;
+    }
+> {
+    private speed = 0; // TODO
+    private progress = 0; // TODO
+    private estimatedStop = 'Not running'; // TODO
+    private logo = playTask; // TODO if state is running, then stopTask is displayed
+    private logoHover = playTaskHover;
+    private displayedLogo = this.logo;
+    public state = {
+        mouseIsEnterTaskCard: false,
+        mouseIsEnterRunTask: false,
+        clickedRunTask: false,
     };
+    private cardBody = cardBodyGeneric;
+
+    private onClickRunTask: () => void = () => {
+        this.setState({
+            clickedRunTask: !this.state.clickedRunTask,
+        });
+        this.logo = this.state.clickedRunTask ? stopTask : playTask;
+        this.logoHover = this.state.clickedRunTask
+            ? stopTaskHover
+            : playTaskHover;
+        this.displayedLogo = this.state.mouseIsEnterRunTask
+            ? this.logoHover
+            : this.logo;
+    };
+
+    private onMouseEnterCard: () => void = () => {
+        this.setState({
+            mouseIsEnterTaskCard: true,
+        });
+        this.cardBody = {
+            ...cardBodyGeneric,
+            boxShadow: '0px 15px 5px 0px #FC6F6F',
+        };
+    };
+
+    private onMouseLeaveCard: () => void = () => {
+        this.setState({
+            mouseIsEnterTaskCard: false,
+        });
+        this.cardBody = cardBodyGeneric;
+    };
+
+    private onMouseEnterRunTask: () => void = () => {
+        this.setState({
+            mouseIsEnterRunTask: true,
+        });
+        this.displayedLogo = this.logoHover;
+    };
+
+    private onMouseLeaveRunTask: () => void = () => {
+        this.setState({
+            mouseIsEnterRunTask: false,
+        });
+        this.displayedLogo = this.logo;
+    };
+
+    private renderTaskInfo = () => {
+        const hashTypeName =
+            this.props.hashTypeId.name.length > 20
+                ? this.props.hashTypeId.name.substring(0, 20) + '...'
+                : this.props.hashTypeId.name;
+        return (
+            <p>
+                {hashTypeName}
+                <br />
+                {this.props.templateTaskId
+                    ? this.props.templateTaskId.name
+                    : ''}
+                <br />
+                {this.props.hashlistId.name}
+                <br />
+                {this.props.options.wordlistId.name}
+                <br />
+            </p>
+        );
+    };
+
     render() {
         return (
-            <div style={this.main}>
-                <div>
-                    <p>{this.props.name}</p>
-                    <p>{this.props.description}</p>
+            <div
+                onMouseEnter={this.onMouseEnterCard}
+                onMouseLeave={this.onMouseLeaveCard}
+                style={this.cardBody}
+            >
+                <div style={topPart}>
+                    <div style={topLeftPart}>
+                        <p style={taskName}>{this.props.name}</p>
+                        <div style={taskSoftInfos}>
+                            <this.renderTaskInfo />
+                        </div>
+                    </div>
+                    <div style={moreDetails}>
+                        <p className="moreDetails">More details</p>
+                    </div>
                 </div>
-                <div></div>
-                <div></div>
+                <div style={bottomBox}>
+                    <div>
+                        <p style={bottomBoxText}>
+                            Speed: {this.speed} H/s
+                            <br />
+                            Progress: {this.progress}
+                            <br />
+                            Estimated end: {this.estimatedStop}
+                        </p>
+                    </div>
+                    <div style={runButton}>
+                        <img
+                            onMouseEnter={this.onMouseEnterRunTask}
+                            onMouseLeave={this.onMouseLeaveRunTask}
+                            onClick={this.onClickRunTask}
+                            src={this.displayedLogo}
+                            alt="Logo"
+                        />
+                    </div>
+                </div>
+                <div style={shadow}></div>
             </div>
         );
     }
 }
-
-export default CardTask;
---
+++ hashkitty-front/src/styles/CardTask.ts
@@ -0,0 +1,80 @@
+import { CSSProperties } from 'react';
+
+export const cardBodyGeneric: CSSProperties = {
+    height: 309,
+    width: 445,
+    border: '2px solid',
+    borderRadius: '8%',
+};
+
+export const topPart: CSSProperties = {
+    display: 'grid',
+    gridTemplateColumns: '2.5fr 1fr',
+    // gridTemplateRows: 'repeat(5, 1fr)',
+    gridColumnGap: '0px',
+    gridRowGap: '0px',
+    gridArea: '1 / 1 / 2 / 6',
+    gap: 0,
+    margin: 0,
+    padding: 0,
+};
+
+export const taskName: CSSProperties = {
+    // display: 'grid',
+    fontFamily: 'InterBold',
+    fontSize: 24,
+    marginBottom: 0,
+};
+
+export const moreDetails: CSSProperties = {
+    margin: 0,
+    paddingTop: 10,
+    textAlign: 'left',
+    gridArea: '1 / 2 / 2 / 3',
+};
+
+export const topLeftPart: CSSProperties = {
+    gridArea: '1 / 1 / 2 / 2',
+    paddingLeft: 40,
+};
+
+export const taskSoftInfos: CSSProperties = {
+    color: '#606060',
+    marginTop: 0,
+    fontFamily: 'InterMedium',
+    fontWeight: 500,
+    fontSize: '16',
+    lineHeight: '19.5px',
+};
+
+export const bottomBox: CSSProperties = {
+    paddingLeft: 40,
+    gridArea: '2 / 1 / 3 / 6',
+    display: 'grid',
+    gridTemplateColumns: '2.5fr 1fr',
+};
+
+export const runButton: CSSProperties = {
+    marginTop: 40,
+    paddingLeft: 10,
+};
+
+export const bottomBoxText: CSSProperties = {
+    fontFamily: 'InterMedium',
+    fontSize: '20px',
+    fontWeight: 500,
+    lineHeight: '30px',
+};
+
+export const shadow: CSSProperties = {
+    // marginLeft: '10%',
+    // marginTop: '-3%',
+    // // position: 'relative',
+    // // justifyContent: 'center',
+    // // alignItems: 'center',
+    // // backgroundColor: 'blue',
+    // boxShadow: '0px 18px 0px -5px #000000',
+    // height: '10%',
+    // width: '80%',
+    // borderRadius: '20px',
+};
