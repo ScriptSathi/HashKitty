07435da20f8e5064b3aefb24cf65a7ce0416048a
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-18 21:07:23 +0100

    Make start hashcat from tasks working fine

--- hashkitty-front/src/pages/HomePage.tsx
+++ hashkitty-front/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { Component, CSSProperties } from 'react';
+import React, { Component } from 'react';
 
 import Navbar from '../components/Navbar';
 import newTask from '../assets/images/newTask.svg';
