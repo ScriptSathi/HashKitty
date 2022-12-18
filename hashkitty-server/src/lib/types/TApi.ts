d12046a7c3e7ec5ada4fed7294658159ca68d7e8
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-18 01:04:32 +0100

    change the way tasks are started and restored to make it more logic with the other apis endpoints

--- hashkitty-server/src/lib/types/TApi.ts
+++ hashkitty-server/src/lib/types/TApi.ts
@@ -12,14 +12,6 @@ export type ThashList = {
     hashs: string[];
 };
 
-export type TExecEndpoint = {
-    wordlist: string;
-    hashList: ThashList;
-    flags: TflagOption[];
-};
-
-export type TRestoreEndpoint = TflagOption<string, 'session'>;
-
 export interface ISslConfig {
     use: boolean;
     key?: string;
@@ -93,3 +85,77 @@ export type TresponseMessagesTypesAgregator =
     | AllowedResponseFormat
     | number
     | void;
+
+// Is the same as ORM Task but with all relations inside instead of foreign keys
+export type TTask = {
+    id: number;
+    name: string;
+    options: Options;
+    hashTypeId: THashType;
+    hashlistId: THashlist;
+    createdAt: string;
+    lastestModification: string;
+    isfinished: number;
+    description?: string;
+    templateTaskId?: TemplateTask;
+    endeddAt?: string | null;
+};
+
+export type Options = {
+    id: number;
+    attackModeId: TAttackMode;
+    breakpointGPUTemperature: number;
+    wordlistId: TWordlist;
+    workloadProfileId: TWorkloadProfile;
+    kernelOpti: Boolean;
+    CPUOnly: Boolean;
+    ruleName?: string;
+    potfileName?: string;
+    maskQuery?: string;
+    maskFilename?: string;
+};
+
+export type TemplateTask = {
+    id: number;
+    name: string;
+    description?: string;
+    createdAt: string;
+    lastestModification: string;
+};
+
+export type THashType = {
+    id: number;
+    typeNumber: number;
+    name: string;
+    description: string;
+};
+
+export type THashlist = {
+    id: number;
+    name: string;
+    description: string;
+    path: string;
+    createdAt: string;
+    lastestModification: string;
+    numberOfCrackedPasswords: number;
+};
+
+export type TWordlist = {
+    id: number;
+    name: string;
+    description: string;
+    path: string;
+};
+
+export type TAttackMode = {
+    id: number;
+    name: string;
+    mode: number;
+};
+
+export type TWorkloadProfile = {
+    id: number;
+    profileId: number;
+    desktopImpact: string;
+    powerConsumation: string;
+};
