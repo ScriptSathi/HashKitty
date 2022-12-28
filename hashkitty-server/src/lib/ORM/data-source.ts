e5ada482882b1cc6ae2a97076dd2dad43a4cb7b1
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-28 15:11:25 +0100

    fix change due to docker add

--- hashkitty-server/src/lib/ORM/data-source.ts
+++ hashkitty-server/src/lib/ORM/data-source.ts
@@ -11,7 +11,7 @@ import { WorkloadProfile } from './entity/WorkloadProfile';
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'mysql',
+    host: 'localhost',
     port: 3306,
     username: 'hashkitty',
     password: 'hashkitty',
