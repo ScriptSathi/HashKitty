adfbf2926c8db88fbf881bedca90e3c65ad4e734
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-28 15:03:00 +0100

    Sending of forms is almost done

--- hashkitty-front/src/types/TComponents.ts
+++ hashkitty-front/src/types/TComponents.ts
@@ -1,7 +1,7 @@
 export type newTaskFormData = {
     formName: string;
     formHashlistName: string;
-    formAttackMode: number;
+    formAttackModeId: number;
     formCpuOnly: boolean;
     formRuleName: string;
     formMaskQuery: string;
@@ -10,4 +10,26 @@ export type newTaskFormData = {
     formWordlistName: string;
     formWorkloadProfile: number;
     formBreakpointGPUTemperature: number;
+};
+
+export type ApiTaskFormData = {
+    name: string;
+    description: string;
+    hashlistId: number;
+    id?: number;
+    templateTaskId?: number;
+    options: ApiOptionsFormData;
+};
+
+export type ApiOptionsFormData = {
+    attackModeId: number;
+    breakpointGPUTemperature: number;
+    wordlistName: string;
+    workloadProfileId: number;
+    kernelOpti: boolean;
+    CPUOnly: boolean;
+    ruleName?: string;
+    potfileName?: string;
+    maskQuery?: string;
+    maskFilename?: string;
 };
