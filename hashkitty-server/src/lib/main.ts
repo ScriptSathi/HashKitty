b26cfcd500ee900f377c23eed5ff6dc8e896287b
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-29 18:31:03 +0100

    add clean way to bootstrap hashkitty

--- hashkitty-server/src/lib/main.ts
+++ hashkitty-server/src/lib/main.ts
@@ -1,30 +1,9 @@
 import 'reflect-metadata';
-import { HttpServer } from './API/HttpServer';
-import { Constants } from './Constants';
-import { AppDataSource } from './ORM/data-source';
-import { logger } from './utils/Logger';
 
-async function main(): Promise<void> {
-    let appDataSource = undefined;
+import { Hashkitty } from './Hashkitty';
 
-    while (appDataSource === undefined) {
-        try {
-            appDataSource = await AppDataSource.initialize();
-        } catch (error) {
-            const sleepTime = 5;
-            logger.error(error);
-            logger.info(
-                `Error while trying to connect to MySQL, retry in ${sleepTime} seconds`
-            );
-            await new Promise<void>(resolve =>
-                setTimeout(() => {
-                    resolve();
-                }, sleepTime * 1000)
-            );
-        }
-    }
-
-    new HttpServer(Constants.defaultApiConfig, appDataSource).listen();
+function main(): void {
+    new Hashkitty().bootstrap();
 }
 
 main();
