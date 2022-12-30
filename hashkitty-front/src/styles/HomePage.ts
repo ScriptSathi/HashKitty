006662916657acc4ff74bb3f317589b7e6e726a3
Tristan d'Audibert <tristan.daudibert@gmail.com>
2022-12-30 15:30:53 +0100

    add ended tasks card, not finnish tho, it misses the task deletion and reupdate of the data

--- hashkitty-front/src/styles/HomePage.ts
+++ hashkitty-front/src/styles/HomePage.ts
@@ -3,40 +3,37 @@ import '../assets/fonts/Inter-Bold.ttf';
 import '../assets/fonts/Inter-Medium.ttf';
 
 export const mainBox: CSSProperties = {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
     display: 'grid',
     gridTemplateColumns: 'minmax(auto, 69%) auto',
     marginTop: 100,
     paddingLeft: 60,
 };
 export const LeftBox: CSSProperties = {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
     border: '1px solid',
     borderLeftStyle: 'none',
     borderTopStyle: 'none',
     borderBottomStyle: 'none',
 };
-export const RightBox: CSSProperties = {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+export const runningTasks: CSSProperties = {
     display: 'grid',
+    gridTemplateColumns: 'minmax(auto, 36%) auto',
+    gap: '45px 20px',
 };
-export const runningTasksTitle: CSSProperties = {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+export const RightBox: CSSProperties = {
     display: 'block',
+    marginLeft: 50,
+};
+export const tasksTitle: CSSProperties = {
     fontFamily: 'InterBold',
     fontSize: 24,
     paddingLeft: 50,
-    height: 50,
-    width: '100%',
 };
 export const cardBody: CSSProperties = {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
     display: 'grid',
     gap: 20,
     gridTemplateColumns: 'minmax(auto, 73%) auto',
 };
 export const newTask: CSSProperties = {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
     height: 310,
     width: 285,
 };
---
+++ hashkitty-front/src/styles/RunnableTaskCard.ts
@@ -0,0 +1,74 @@
+import { CSSProperties } from 'react';
+
+export const cardBodyGeneric: CSSProperties = {
+    height: 309,
+    width: 445,
+    border: '2px solid',
+    borderRadius: '8%',
+};
+
+export const topPart: CSSProperties = {
+    display: 'grid',
+    gridTemplateColumns: '2.5fr 1fr',
+    gridColumnGap: '0px',
+    gridRowGap: '0px',
+    gridArea: '1 / 1 / 2 / 6',
+    gap: 0,
+    margin: 0,
+    padding: 0,
+};
+
+export const taskName: CSSProperties = {
+    fontFamily: 'InterBold',
+    fontSize: 24,
+    marginBottom: 0,
+};
+
+export const moreDetails: CSSProperties = {
+    margin: 0,
+    paddingTop: 10,
+    textAlign: 'left',
+    gridArea: '1 / 2 / 2 / 3',
+};
+
+export const topLeftPart: CSSProperties = {
+    gridArea: '1 / 1 / 2 / 2',
+    paddingLeft: 40,
+};
+
+export const taskSoftInfos: CSSProperties = {
+    color: '#606060',
+    marginTop: 0,
+    fontFamily: 'InterMedium',
+    fontWeight: 500,
+    fontSize: '16',
+    lineHeight: '19.5px',
+};
+
+export const bottomBox: CSSProperties = {
+    paddingLeft: 40,
+    gridArea: '2 / 1 / 3 / 6',
+    display: 'grid',
+    gridTemplateColumns: '2.5fr 1fr',
+};
+
+export const runButton: CSSProperties = {
+    cursor: 'pointer',
+    marginTop: 40,
+    paddingLeft: 10,
+};
+
+export const bottomBoxText: CSSProperties = {
+    fontFamily: 'InterMedium',
+    fontSize: '20px',
+    fontWeight: 500,
+    lineHeight: '30px',
+};
+
+export const cardOnStartError: CSSProperties = {
+    marginLeft: '-3%',
+    marginTop: '5%',
+    position: 'absolute',
+    fontSize: '20px',
+    color: 'red',
+};
