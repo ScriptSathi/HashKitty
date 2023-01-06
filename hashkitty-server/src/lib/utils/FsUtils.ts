1a60ea450d67113eebb586c7dba0adfba8384461
Tristan D'audi <tristan.daudibert@gmail.com>
2023-01-06 17:34:08 +0100

    merging FileManager.ts and FsUtils.ts

--- hashkitty-server/src/lib/utils/FsUtils.ts
+++ hashkitty-server/src/lib/utils/FsUtils.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs-extra';
+import { logger } from './Logger';
 
 export class FsUtils {
     public static listFileInDir(path: string): string[] {
@@ -13,5 +14,30 @@ export class FsUtils {
             }
         }
         return [];
+    }
+
+    public directoryWhereToSavedFile: string;
+    public filePath = '';
+
+    constructor(pathToSavedFile: string) {
+        this.directoryWhereToSavedFile = pathToSavedFile;
+    }
+
+    public async createHashFile(
+        filename: string,
+        data: string[]
+    ): Promise<void> {
+        if (!fs.existsSync(this.directoryWhereToSavedFile)) {
+            await fs.ensureDir(this.directoryWhereToSavedFile);
+        }
+        this.filePath = `${this.directoryWhereToSavedFile}/${filename}`;
+        await fs.writeFile(this.filePath, data.join('\n'));
+        logger.debug(`Writing hash file at location ${this.filePath}`);
+    }
+
+    public async countLineInFile(path: string): Promise<number> {
+        const file = await fs.readFile(path, 'utf-8');
+        console.log(file.toString());
+        return 1;
     }
 }
