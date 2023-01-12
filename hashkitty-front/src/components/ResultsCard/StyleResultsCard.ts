8754169d72745be5e95dea1613a52716c511cbdb
Tristan D'audi <tristan.daudibert@gmail.com>
2023-01-12 23:50:38 +0100

    wip display results

---
+++ hashkitty-front/src/components/ResultsCard/StyleResultsCard.ts
@@ -0,0 +1,22 @@
+import { CSSProperties } from 'react';
+
+export const cardBody: CSSProperties = {
+    backgroundColor: 'white',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    border: '2px solid',
+    borderRadius: '50px',
+};
+
+export const contentBody: CSSProperties = {
+    margin: '10%',
+    height: '80%',
+    width: '90%',
+};
+
+export const passwdsTxt: CSSProperties = {
+    lineHeight: 0,
+    fontSize: 16,
+    fontFamily: 'InterMedium',
+};
