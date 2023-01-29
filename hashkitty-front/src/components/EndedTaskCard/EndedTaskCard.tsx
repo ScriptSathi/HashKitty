ad672e077f03e0a25cb101ea866ea5af4ee25089
Tristan D'audi <tristan.daudibert@gmail.com>
2023-01-29 17:42:44 +0100

    refacto ended tasks

--- hashkitty-front/src/components/EndedTaskCard/EndedTaskCard.tsx
+++ hashkitty-front/src/components/EndedTaskCard/EndedTaskCard.tsx
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
 import duration from 'humanize-duration';
 
-import { TTask } from '../types/TypesORM';
-import {
-    bottomBox,
-    bottomBoxText,
-    cardBodyGeneric,
-    cardOnStartError,
-    moreDetails,
-    deleteButton,
-    taskName,
-    taskSoftInfos,
-    topLeftPart,
-    topPart,
-    resultsButton,
-} from '../styles/EndedTaskCard';
-import '../assets/styles/main.scss';
-import trash from '../assets/images/trash.svg';
-import resultsLogo from '../assets/images/results.png';
-import { Constants } from '../Constants';
-import BackgroundBlur from './BackgroundBlur/BackGroundBlur';
-import ResultsCard from './ResultsCard/ResultsCard';
+import { TTask } from '../../types/TypesORM';
+import '../../assets/styles/main.scss';
+import './EndedTaskCard.scss';
+import trash from '../../assets/images/trash.svg';
+import resultsLogo from '../../assets/images/results.png';
+import { Constants } from '../../Constants';
+import BackgroundBlur from '../BackgroundBlur/BackGroundBlur';
+import ResultsCard from '../ResultsCard/ResultsCard';
 
 type EndedTaskCardState = {
-    mouseIsEnterTaskCard: boolean;
-    mouseIsEnterRunTask: boolean;
     moreDetailsClicked: boolean;
     clickedRunTask: boolean;
     onErrorStart: string;
@@ -44,8 +30,6 @@ export default class EndedTaskCard extends Component<
     EndedTaskCardState
 > {
     public state: EndedTaskCardState = {
-        mouseIsEnterTaskCard: false,
-        mouseIsEnterRunTask: false,
         moreDetailsClicked: false,
         toggledResults: false,
         clickedRunTask: this.props.isRunning,
@@ -57,7 +41,6 @@ export default class EndedTaskCard extends Component<
 
     private interval: number;
     private displayedLogo = trash;
-    private cardBody = cardBodyGeneric;
 
     public componentDidMount() {
         this.setNewDurationTime();
@@ -70,56 +53,37 @@ export default class EndedTaskCard extends Component<
 
     public render() {
         return (
-            <div
-                onMouseEnter={this.onMouseEnterCard}
-                onMouseLeave={this.onMouseLeaveCard}
-                style={this.cardBody}
-            >
-                <div style={topPart}>
-                    <div style={topLeftPart}>
-                        <p style={taskName}>{this.props.name}</p>
-                        <div style={taskSoftInfos}>
+            <div className="cardBodyGeneric">
+                <div className="topPart">
+                    <div className="topLeftPart">
+                        <p className="taskName">{this.props.name}</p>
+                        <div className="taskSoftInfos">
                             <this.renderTaskInfo />
                         </div>
                     </div>
-                    <div style={moreDetails}>
+                    <div className="moreDetails">
                         <img
-                            style={
-                                this.state.isMouseOverResultBtn
-                                    ? {
-                                          ...resultsButton,
-                                          filter: 'invert(55%) sepia(6%) saturate(5000%) hue-rotate(314deg) brightness(100%) contrast(200%)',
-                                      }
-                                    : resultsButton
-                            }
                             onClick={this.displayResults}
-                            onMouseEnter={this.mouseOverResBtn}
-                            onMouseLeave={this.mouseOverResBtn}
                             src={resultsLogo}
+                            className="resultsButton"
                             alt="Displaying results"
                         ></img>
-                        <p style={cardOnStartError}>
+                        <p className="cardOnStartError">
                             {this.state.onErrorStart}
                         </p>
                     </div>
                 </div>
-                <div style={bottomBox}>
-                    <p style={bottomBoxText}>
+                <div className="bottomBox">
+                    <p className="bottomBoxText">
                         <br />
                         Ended since: {this.state.endedSince}
                         <br />
                         Nb of cracked passwords:{' '}
                         {this.props.hashlistId.numberOfCrackedPasswords}
                     </p>
-                    <div style={deleteButton}>
+                    <div className="deleteButton">
                         <img
-                            className={
-                                this.state.mouseIsEnterRunTask
-                                    ? 'deleteTask'
-                                    : ''
-                            }
-                            onMouseEnter={this.onMouseEnterRunTask}
-                            onMouseLeave={this.onMouseLeaveRunTask}
+                            className="deleteTask"
                             onClick={this.onClickDeleteTask}
                             src={this.displayedLogo}
                             alt="Logo"
@@ -154,17 +118,6 @@ export default class EndedTaskCard extends Component<
         });
     }
 
-    private displayErrorMessageOnDeleteTask(): void {
-        this.setState({ onErrorStart: 'An error occured' });
-        setTimeout(() => this.setState({ onErrorStart: '' }), 3000);
-    }
-
-    private mouseOverResBtn = (): void => {
-        this.setState({
-            isMouseOverResultBtn: !this.state.isMouseOverResultBtn,
-        });
-    };
-
     private fetchDeleteTask(isClicked: boolean): void {
         if (isClicked) {
             const requestOptions = {
@@ -191,35 +144,6 @@ export default class EndedTaskCard extends Component<
 
     private onClickDeleteTask: () => void = () => {
         this.fetchDeleteTask(!this.state.clickedRunTask);
-    };
-
-    private onMouseEnterCard: () => void = () => {
-        this.setState({
-            mouseIsEnterTaskCard: true,
-        });
-        this.cardBody = {
-            ...cardBodyGeneric,
-            boxShadow: '0px 5px 5px 0px #FC6F6F',
-        };
-    };
-
-    private onMouseLeaveCard: () => void = () => {
-        this.setState({
-            mouseIsEnterTaskCard: false,
-        });
-        this.cardBody = cardBodyGeneric;
-    };
-
-    private onMouseEnterRunTask: () => void = () => {
-        this.setState({
-            mouseIsEnterRunTask: true,
-        });
-    };
-
-    private onMouseLeaveRunTask: () => void = () => {
-        this.setState({
-            mouseIsEnterRunTask: false,
-        });
     };
 
     private renderTaskInfo = () => {
