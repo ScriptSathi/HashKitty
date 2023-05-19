fb4f061399d7afa2408fe7e4dbe4df11ff49a927
Tristan d'audibert <tristan.daudibert@gmail.com>
2023-05-19 12:41:42 +0200

    turn error page to dark theme

--- hashkitty-front/src/pages/ErrorPage/ErrorPage.tsx
+++ hashkitty-front/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,23 +1,29 @@
+import { useContext } from 'react';
 import { useRouteError, useNavigate } from 'react-router-dom';
-import Navbar from '../../components/NavBar/NavBar';
 import { TuseRouteError } from '../../types/THooks';
 import logo from '../../assets/images/CryingKitty.svg';
 import Button from '../../components/ui/Buttons/Button';
+import Frame from '../../components/Frame/Frame';
 
 import './ErrorPage.scss';
+import ColorModeContext from '../../App/ColorModeContext';
 
 export default function ErrorPage() {
    const { statusText } = useRouteError() as TuseRouteError;
+   const {
+      theme: { isDarkTheme },
+   } = useContext(ColorModeContext);
    const redirectTo = useNavigate();
    document.body.style.overflow = 'hidden';
    return (
-      <div>
-         <div>
-            <Navbar />
-         </div>
+      <Frame>
          <div className="DivFlex border-solid">
             <div className="ErrorLeftBox">
-               <img className="cryingCat" src={logo} alt="Logo" />
+               <img
+                  className={`cryingCat ${isDarkTheme && 'invertImg'}`}
+                  src={logo}
+                  alt="Logo"
+               />
             </div>
             <div className="ErrorRightBox">
                <p className="errorText">404 {statusText}</p>
@@ -30,6 +36,6 @@ export default function ErrorPage() {
                </div>
             </div>
          </div>
-      </div>
+      </Frame>
    );
 }
